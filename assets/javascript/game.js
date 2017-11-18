fccc4f5c6969d5ad3d7f08468d3b6269c7aaefa5
Caleb Rose <clrose86@gmail.com>
2017-11-17 20:21:56 -0600

    updated js 3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,72 +9,70 @@ document.getElementById("losses").innerHTML = ("Losses: " + losses);
 var wrongGuess = 10;
 
 
-// var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 // Create an array of words
-var wordList = ["Rick Deckard", 
-				"Gaff", 
-				"Rachel", 
-				"Roy Batty", 
-				"Pris Stratton", 
-				"Replicant", 
-				"Tyrell Corporation", 
-				"Blush Response", 
-				"Vangelis", 
-				"Ridley Scott"
+var wordList = ["Mike", 
+				"Dustin", 
+				"Will", 
+				"Lucas", 
+				"Eleven", 
+				"Hopper",
+				"Joyce", 
+				"Steve", 
+				"Nancy", 
+				"Jonathan", 
+				"Barb",
 			];
+			
+
 
 // begin game
-document.onkeydown = function(event) {
+document.onkeyup = function(event){
 
-// Choose a word randomly 
-var choice = Math.floor(Math.random() * wordList.length);
-var chosenWord = wordList[choice].toLowerCase();
+	// Choose a word randomly 
+	var choice = Math.floor(Math.random() * wordList.length);
+	var chosenWord = wordList[choice].toLowerCase();
+	// Create underscores based on the length of word
+	var underscore = [];
+	var str = "";
 
-// Create underscores based on the length of word
-var underscore = [];
-var str = "";
-function generateUnderscore(chosenWord) {
-	for (var i =0; i < chosenWord.length; i++) {
-		underscore.push("_");
-		str += "_";
-	}
-	return underscore;
-};
-// console.log(underscore.join(""));
-console.log("chosen word "+chosenWord);
-console.log(generateUnderscore(chosenWord));
-console.log("str "+str);	
-console.log("under "+underscore.join(""));
-document.getElementById("answer").innerHTML = (underscore.join(" ")) ;	
+	function generateWord(chosenWord) {
+		for (var i =0; i < chosenWord.length; i++) {
+			underscore.push("_");
+			str += "_";
+		}
+		return underscore;
+	};
+	console.log(underscore.join(""));
+	console.log("chosen word "+chosenWord);
+	console.log(generateWord(chosenWord));
+	console.log("str "+str);	
+	console.log("under "+underscore.join(""));
+	document.getElementById("answer").innerHTML = (underscore.join(" "));	
 
-// array of alphabet for user’s input 
 // Check users guess
 // Check if the guess is correct
 // If guess is right take out of available choices and add show letter on screen. Take letter out of array of possible choices. 
 // If guess wrong: take out of list of possible choices and animate hangman progressively.
+	document.onkeyup = function(event) {
+		var userInput = event.key.toLowerCase();
+		console.log(userInput);
+			if (chosenWord.indexOf(userInput) != -1) {
+				for (j = 0; j<chosenWord.length; j++){
+					if (chosenWord[j] == userInput){
+						underscore[j] = chosenWord[j];
+						document.getElementById("answer").innerHTML = (underscore.join(" "));
+					}
+				};
 
-		// var userInput = event.key.toLowerCase();
-		// console.log(userInput);
-	
-
-	if (chosenWord.indexOf(userInput) != -1) {
-		for (j = 0; j<chosenWord.length; j++){
-			if (chosenWord[j] == userInput){
-				underscore[j] = chosenWord[j];
-				document.getElementById("answer").innerHTML = (underscore.join(" "));
-				}
+			} else {
+				wrongGuess--;
+				console.log(wrongGuess);
+				document.getElementById("guesses").innerHTML = ("Guesses left: " + wrongGuess);
 			};
-		} else {
-			wrongGuess--;
-			console.log(wrongGuess);
-			document.getElementById("guesses").innerHTML = ("Guesses left: " + wrongGuess);
-		};
 	};
-}
+};
 
-
-
- 
+} 
 
 // if word completed add to win column and show picture of right answer
 
